fe13747f09a023b01e7cc035b2e82d1781bbb69e
Ciervos <mlujafo@gmail.com>
2021-08-14 03:30:46 -0300

    función para calcular cantidad de estrellas según promedio de votos

--- src/components/Stars/index.js
+++ src/components/Stars/index.js
@@ -1,12 +1,47 @@
+import {useState,useEffect} from 'react';
 import './style.scss';
 import { Star,StarFill,StarHalf } from 'react-bootstrap-icons';
 
 function Stars(props) {
-  console.log(props)
-  
+  console.log(props.data)
+  let average = props.data;
+  let fullnum = parseInt(props.data) 
+  const [totalstars,setTotalStars] = useState(10);
+  const [halfstar,setHalfStar] = useState(false);
+
+  useEffect(() => {
+   //Esta función nos ayuda a saber cuantas estrellas necesitaremos para llenar las 10 estrellas
+   
+    totalOfStars()
+
+  }, []);
+
+  function totalOfStars(){
+   //Revisa si el número de promedio es entero o necesita que agreguemos una estrella a medio llenar
+   
+   if(average%1!==0){
+    setTotalStars(9)
+    setHalfStar(true)
+   }
+
+  }
+
+
     return <div>
-           Estrellitas
-        <span className="stars-average">{props.data}</span>
+        
+        {[...Array(fullnum)].map((x, key) =>
+        <StarFill key={`Full star ${key}`}/>
+        )
+    
+        }
+       {halfstar ? <StarHalf/>:false}
+       {[...Array(totalstars-fullnum)].map((x, key) =>
+        <Star key={`star ${key}`}/>
+        )
+       }
+
+         
+        <span className="stars-average">{average}</span>
       </div>
   }
   
